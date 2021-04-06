7929cf10307f9269ec284fe303bc9b0c8f87853d
YanshikRajput <75445620+YanshikRajput@users.noreply.github.com>
2021-04-07 02:01:25 +0530

    Delete Angular Apps/src directory

--- Angular Apps/src/app/productpipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ProductpipePipe } from './productpipe.pipe';
-
-describe('ProductpipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new ProductpipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
