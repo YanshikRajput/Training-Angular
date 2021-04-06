344c759f82af47baf150a3e3f0d57bcb1a3af6e4
YanshikRajput <yanshikrajput99@gmail.com>
2021-04-06 09:42:23 +0530

    Angular Updated

---
+++ Angular/src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { ErrorHandler, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { TshirtdatabindingComponent } from './tshirtdatabinding/tshirtdatabinding.component';
+import { HelloComponent } from './hello/hello.component';
+import { ProductComponent } from './product/product.component';
+import { PurchaseComponent } from './purchase/purchase.component';
+import { PurchaseorderComponent } from './purchaseorder/purchaseorder.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatExpansionModule} from '@angular/material/expansion';
+import { Child1Component } from './child1/child1.component';
+import { Child2Component } from './child2/child2.component';
+import { ChangecolorDirective } from './changecolor.directive';
+import { AppRoutingModule } from './app-routing.module';
+import { AttributeDirectiveComponent } from './attribute-directive/attribute-directive.component';
+import { HomeComponent } from './home/home.component';
+import { GenerateerrorComponent } from './generateerror/generateerror.component';
+import { MyerrorhandelerService } from './myerrorhandeler.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TshirtdatabindingComponent,
+    HelloComponent,
+    ProductComponent,
+    PurchaseComponent,
+    PurchaseorderComponent,
+    Child1Component,
+    Child2Component,
+    ChangecolorDirective,
+    AttributeDirectiveComponent,
+    HomeComponent,
+    GenerateerrorComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatExpansionModule
+  ],
+  providers: [{provide:ErrorHandler,useClass:MyerrorhandelerService}],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
